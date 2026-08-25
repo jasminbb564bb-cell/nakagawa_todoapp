d61862059923194a7ab86acbcebbc28e90644f17
LAPTOP-TT9UVE9D\academia <202605_aw_nakagawa@x-ide.sakura.ne.jp>
2026-08-25 10:35:58 +0900

    一覧画面をつくる

--- src/main/java/com/example/todoapp/HomeController.java
+++ src/main/java/com/example/todoapp/HomeController.java
@@ -1,5 +1,7 @@
 package com.example.todoapp;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +9,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
+    private final TodoMapper todoMapper;
+
+    public HomeController(TodoMapper todoMapper) {
+        this.todoMapper = todoMapper;
+    }
+
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("title", "やること管理");
+        model.addAttribute("title", "繧・ｋ縺薙→邂｡逅・);
         return "index";
+    }
+
+    @GetMapping("/todos")
+    public String todos(Model model) {
+        List<Todo> todos = todoMapper.findAll();
+        model.addAttribute("todos", todos);
+        return "todos";
     }
 }
