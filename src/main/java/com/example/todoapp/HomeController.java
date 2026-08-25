690b117c41a87af6fb0e1072e761b72a3c4ce295
LAPTOP-TT9UVE9D\academia <202605_aw_nakagawa@x-ide.sakura.ne.jp>
2026-08-25 11:39:59 +0900

    登録完了

--- src/main/java/com/example/todoapp/HomeController.java
+++ src/main/java/com/example/todoapp/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class HomeController {
@@ -31,8 +32,9 @@ public class HomeController {
     }
 
     @PostMapping("/todos")
-    public String save(@ModelAttribute("todo") Todo todo) {
+    public String save(@ModelAttribute("todo") Todo todo, RedirectAttributes redirectAttributes) {
         todoMapper.insert(todo);
+        redirectAttributes.addFlashAttribute("message", "登録しました");
         return "redirect:/todos";
     }
 
